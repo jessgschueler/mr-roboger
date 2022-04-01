001f78451e5f5e19b80bc385cb183b3b5e6aeeec
Jess Schueler <jessicagrace.schueler@gmail.com>
2022-04-01 13:15:42 -0700

    It will replace any number containing '1' with 'Beep!'

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,30 @@ function mrRobo(number) {
   for (let i = 0; i <= number; i++) {
     inputArray.push(i.toString());
    }
-  for (let i = 0; i <=inputArray.length; i++) {
-    let elementValue = inputArray[i] 
-    if (elementValue === '1' || elementValue === '1') {
-      inputArray[i] = 'Beep!'
+   inputArray.forEach(function(element, index) {
+    if (element.includes('1')) {
+      inputArray.splice(index, 1, 'Beep!')
+      
     }
+    });
+    return inputArray
   }
-  for (let i = 0; i <=inputArray.length; i++) {
-    if (inputArray[i] === '2') {
-        inputArray[i] = 'Boop!'
-    }
-  }
-  for (let i = 0; i <=inputArray.length; i++) {
-    if (inputArray[i] === '3') {
-      inputArray[i] = "Won't you be my neighbor?"
-    }
-  }
-  return inputArray
-}
+
+//   for (let i = 0; i <= inputArray.length; i++) {
+//     let elementValue = inputArray[i]
+//     if (elementValue === '1') {
+//       inputArray[i] = 'Beep!'
+//     }
+//   }
+//   for (let i = 0; i <= inputArray.length; i++) {
+//     if (inputArray[i] === '2') {
+//         inputArray[i] = 'Boop!'
+//     }
+//   }
+//   for (let i = 0; i <= inputArray.length; i++) {
+//     if (inputArray[i] === '3') {
+//       inputArray[i] = "Won't you be my neighbor?"
+//     }
+//   }
+//   return inputArray
+// }
