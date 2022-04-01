3bbc7753e7d7f291f12441cd7f89395ab1525bbe
Jess Schueler <jessicagrace.schueler@gmail.com>
2022-04-01 13:27:11 -0700

    It will replace any any number containing '2' with 'Boop!' and any number containing '3' with 'Won't you be my neighbor?'

--- README.md
+++ README.md
@@ -22,4 +22,12 @@ Expected Output: ['0','Beep!','Boop!',"Won't you be my neighbor?", '4']
 
 Test: It will replace any number containing '1' with 'Beep!'
 Code: mrRobo(10)
-Expected Output: ['0','Beep!','Boop!',"Won't you be my neighbor?", '4','5','6','7','8','9','Beepe!']
+Expected Output: ['0','Beep!','Boop!',"Won't you be my neighbor?", '4','5','6','7','8','9','Beep!']
+
+Test: It will replace any number containing '2' with 'Boop!'
+Code: mrRobo(12)
+Expected Output: ['0','Beep!','Boop!',"Won't you be my neighbor?", '4','5','6','7','8','9','Beep!','Boop!']
+
+Test: It will replace any number containing '3' with 'Won't you be my neighbor?'
+Code: mrRobo(13)
+Expected Output: ['0','Beep!','Boop!',"Won't you be my neighbor?", '4','5','6','7','8','9','Beep!','Boop!','Won't you be my neighbor?']
--- js/scripts.js
+++ js/scripts.js
@@ -6,29 +6,22 @@ function mrRobo(number) {
     inputArray.push(i.toString());
    }
    inputArray.forEach(function(element, index) {
+    if (element.includes('3')) {
+      inputArray.splice(index, 1, "Won't you be my neighbor?")
+    }
+    });
+    inputArray.forEach(function(element, index) {
+      if (element.includes('2')) {
+        inputArray.splice(index, 1, 'Boop!')
+      }
+      });
+   inputArray.forEach(function(element, index) {
     if (element.includes('1')) {
       inputArray.splice(index, 1, 'Beep!')
-      
     }
     });
     return inputArray
   }
 
-//   for (let i = 0; i <= inputArray.length; i++) {
-//     let elementValue = inputArray[i]
-//     if (elementValue === '1') {
-//       inputArray[i] = 'Beep!'
-//     }
-//   }
-//   for (let i = 0; i <= inputArray.length; i++) {
-//     if (inputArray[i] === '2') {
-//         inputArray[i] = 'Boop!'
-//     }
-//   }
-//   for (let i = 0; i <= inputArray.length; i++) {
-//     if (inputArray[i] === '3') {
-//       inputArray[i] = "Won't you be my neighbor?"
-//     }
-//   }
-//   return inputArray
-// }
+// UI //
+
