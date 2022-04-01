a2a46d066f01716bc80998c8139e624eb91ecd99
Jess Schueler <jessicagrace.schueler@gmail.com>
2022-04-01 09:21:20 -0700

    Return array of numbers from 0- user's inputted number.

---
+++ README.md
@@ -0,0 +1,6 @@
+Describe: mrRobo()
+
+Test: It will return an array of numbers from 0 - user's inputted number.
+Code: mrRobo(4)
+Expected Output: [0,1,2,3,4] 
+
---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Business Logic //
+
+function mrRobo(number) {
+  let inputArray = [];
+  for (let i = 0; i <= number; i++) {
+    inputArray.push(i);
+  }
+  return inputArray
+}
+
